8efb020165fab5e6e70082283137f1a2fa6b6b56
Alcatraz Lynn <lym0104@126.com>
2020-07-15 12:12:12 +0900

    Updated kadai3.py

--- ScienceCalculation/5/kadai3.py
+++ ScienceCalculation/5/kadai3.py
@@ -3,8 +3,10 @@ import scipy.stats as st
 import matplotlib.pyplot as plt
 
 
-SampleNum = 1000000
+SampleNum = 100000
 sum = 0
+inRange = []
+outRange = []
 
 for i in range(0,SampleNum):
     x = np.random.rand(1)
@@ -12,6 +14,13 @@ for i in range(0,SampleNum):
 
     if (np.square(x) + np.square(y)) < 1:
         sum = sum + 1
+        inRange.append(tuple((x, y)))
+    else:
+        outRange.append(tuple((x, y)))
 
+plt.axis('equal')
+plt.scatter(*zip(*inRange), c='r', s=1)
+plt.scatter(*zip(*outRange), c='b', s=1)
+plt.show()
 P = sum * 4 / SampleNum
 print(P)
